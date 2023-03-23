a45b926fa86f20e22c5ef219ba9c1a8f12ce5379
Sophea Seng <sopheaseng@Sopheas-MacBook-Pro.local>
2023-03-23 14:58:28 +0700

    P Ton

--- lib/screen/listPage.dart
+++ lib/screen/listPage.dart
@@ -2,10 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:projrct_application/screen/mainPage.dart';
 
-
-
 class ListPage extends StatefulWidget {
-  ListPage({Key? key}) : super(key: key);
+  int counter = 0;
+  ListPage({Key? key, required this.counter}) : super(key: key);
 
   @override
   State<ListPage> createState() => _ListPageState();
@@ -20,36 +19,88 @@ class _ListPageState extends State<ListPage> {
         children: [
           //Showing the header
           Container(
-            child: Container(
-              margin: const EdgeInsets.only(top: 30, bottom: 15),
-              padding: const EdgeInsets.only(left: 0, right: 30),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  TextButton.icon(
-                    onPressed: () => {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => MainPage()),
-                      )
-                    },
-                    icon: Icon(
-                      Icons.chevron_left_outlined,
-                      color: Color.fromRGBO(135, 109, 245, 1),
-                      size: 100,
-                    ),
-                    label: Text(
-                      '',
-                    ),
-                  ),Text('List',style: TextStyle(fontSize: 50.0),),button(context),
-                ],
-              ),
+            margin: const EdgeInsets.only(top: 30, bottom: 15),
+            padding: const EdgeInsets.only(left: 0, right: 30),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                TextButton.icon(
+                  onPressed: () => {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => MainPage()),
+                    )
+                  },
+                  icon: Icon(
+                    Icons.chevron_left_outlined,
+                    color: Color.fromRGBO(135, 109, 245, 1),
+                    size: 100,
+                  ),
+                  label: Text(
+                    '',
+                  ),
+                ),
+                Text(
+                  'List',
+                  style: TextStyle(fontSize: 50.0),
+                ),
+              ],
             ),
           ),
           //Showing The body
           Expanded(
               child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Center(
+                  child: Card(
+                    elevation: 5,
+                    margin: const EdgeInsets.all(20),
+                    child: Column(
+                      //mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: <Widget>[
+                            Container(
+                              padding: EdgeInsets.only(top: 20, bottom: 20),
+                              child: Image.asset(
+                                "assets/image/tesla.jpg",
+                                height: 100.0,
+                                width: 120.0,
+                                fit: BoxFit.cover,
+                                alignment: Alignment.topCenter,
+                              ),
+                            ),
+                            Column(
+                              children: [
+                                Container(
+                                  alignment: Alignment.topLeft,
+                                  child: Text(
+                                      "Product x " + widget.counter.toString()),
+                                ),
+                                IconButton(
+                                  icon: const Icon(
+                                    Icons.delete,
+                                    size: 40,
+                                    color: Colors.red,
+                                  ),
+                                  onPressed: () {
+                                    // ...
+                                  },
+                                ),
+                                const SizedBox(width: 8),
+                              ],
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                buttonNext(context),
+              ],
+            ),
             //child: FoodPageBody(),
           ))
         ],
@@ -58,26 +109,26 @@ class _ListPageState extends State<ListPage> {
   }
 }
 
-  @override
-  Widget button(BuildContext context) {
-    return TextButton(
-      onPressed: () => showDialog<String>(
-        context: context,
-        builder: (BuildContext context) => AlertDialog(
-          // title: const Text('AlertDialog Title'),
-          // content: const Text('AlertDialog description'),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () => Navigator.pop(context, 'Cancel'),
-              child: const Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () => Navigator.pop(context, 'OK'),
-              child: const Text('OK'),
-            ),
-          ],
-        ),
+@override
+Widget buttonNext(BuildContext context) {
+  return TextButton(
+    onPressed: () => showDialog<String>(
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        // title: const Text('AlertDialog Title'),
+        // content: const Text('AlertDialog description'),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () => Navigator.pop(context, 'Cancel'),
+            child: const Text('Cancel'),
+          ),
+          TextButton(
+            onPressed: () => Navigator.pop(context, 'OK'),
+            child: const Text('OK'),
+          ),
+        ],
       ),
-      child: const Text('Next'),
-    );
-  }
+    ),
+    child: const Text('Next'),
+  );
+}
--- lib/screen/mainPage.dart
+++ lib/screen/mainPage.dart
@@ -4,8 +4,6 @@ import 'package:projrct_application/screen/listPage.dart';
 import 'package:projrct_application/DataModel/ProductDataModel.dart';
 import 'package:flutter/services.dart' as rootBundle;
 
-
-
 class MainPage extends StatefulWidget {
   MainPage({Key? key}) : super(key: key);
 
@@ -14,49 +12,109 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  int _counter = 0;
   // final databaseRef = FirebaseDatabase.instance.reference();
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter <= 0 ? _counter = 0 : _counter--;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    body: Container(
-      margin: const EdgeInsets.only(top: 50, bottom: 15),
-      alignment: Alignment.topCenter,
-      padding: EdgeInsets.all(15),
-      child: Wrap(children: [
-        Padding(
-          padding: EdgeInsets.all(5),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(10.0), //add border radius
-            child: Image.asset(
-              "assets/image/tesla.jpg",
-              height: 300.0,
-              width: 400.0,
-              fit: BoxFit.cover,
-              alignment: Alignment.topCenter,
+      body: Container(
+        margin: const EdgeInsets.only(top: 50, bottom: 15),
+        // alignment: Alignment.topCenter,
+        // padding: EdgeInsets.all(15),
+        child: Column(children: [
+          //SizedBox(height: MediaQuery.of(context).size.height * 0.10),
+          Padding(
+            padding: EdgeInsets.all(5),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10.0), //add border radius
+              child: Image.asset(
+                "assets/image/tesla.jpg",
+                height: 300.0,
+                width: 400.0,
+                fit: BoxFit.cover,
+                alignment: Alignment.topCenter,
+              ),
             ),
           ),
-        ),Text('Prodouct',style: TextStyle(fontSize: 50.0),),button(context),
-      ]),
-    ),
-    //Center
+          Container(
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Text(
+                'Prodouct',
+                style: TextStyle(fontSize: 50.0),
+              ),
+            ),
+          ),
+          Container(
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Text(
+                "Information product 5.00\$",
+                style: TextStyle(fontSize: 30.0),
+              ),
+            ),
+          ),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.30),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FloatingActionButton(
+                heroTag: "btn1",
+                onPressed: _decrementCounter,
+                tooltip: 'decrement',
+                child: Icon(Icons.remove),
+              ),
+              SizedBox(width: 10),
+              Text(
+                _counter.toString(),
+                style: TextStyle(fontSize: 15),
+              ),
+              SizedBox(width: 10),
+              FloatingActionButton(
+                heroTag: "btn2",
+                onPressed: _incrementCounter,
+                tooltip: 'Increment',
+                child: Icon(Icons.add),
+              ),
+            ],
+          ),
+          SizedBox(height:20),
+          button(context)
+        ]),
+      ),
+      //Center
     );
   }
-}
 
   Widget button(BuildContext context) {
-    return OutlinedButton(
-       style: OutlinedButton.styleFrom(
-       backgroundColor: Color.fromRGBO(201, 241, 253, 0.784), //<-- SEE HERE
-  ),
+    return Center(
+        child: OutlinedButton(
+      style: OutlinedButton.styleFrom(
+        backgroundColor: Color.fromRGBO(201, 241, 253, 0.784), //<-- SEE HERE
+      ),
       onPressed: () {
-        debugPrint('push Page List');
-         Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => ListPage()),
-                      );
+        //debugPrint('push Page List');
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => ListPage(counter:_counter)),
+        );
       },
-      child: const Text('Next',style: TextStyle(color: Colors.black,fontSize: 40),),
-    );
+      child: Text(
+        (_counter*5).toString()+".00 \$",
+        style: TextStyle(color: Colors.black, fontSize: 40),
+      ),
+    ));
   }
-
+}
